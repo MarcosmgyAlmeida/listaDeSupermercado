2ea0131e212c0e1da2def5108c50d945e72424ab
Marcos Antônio de Almeida <marcosantoniomgy@gmail.com>
2023-07-24 12:18:17 -0300

    Meu commit inicial

---
+++ src/test/java/br/com/marcosalmeida/listadesupermercado/ListaDeSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosalmeida.listadesupermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeSupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
